4a0dca688640363e545e0eb7a379842f9536ea12
Endie <endie@local.com>
2022-08-01 20:25:05 +0100

    Adding initial files

---
+++ src/main/java/com/endie/avizandum/MaintenanceApplication.java
@@ -0,0 +1,19 @@
+package com.endie.avizandum;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MaintenanceApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceApplication.class, args);
+		
+		logger.info("Application Started");
+	}
+
+}
