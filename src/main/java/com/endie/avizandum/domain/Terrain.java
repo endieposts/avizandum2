0a6ec7a217485fe68837cf5c1ead99b9e4e3b9bf
Endie <endie@local.com>
2022-08-03 19:51:26 +0100

    add terrain class

---
+++ src/main/java/com/endie/avizandum/domain/Terrain.java
@@ -0,0 +1,40 @@
+package com.endie.avizandum.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Terrain {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String Name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public Terrain(long id, String name) {
+		super();
+		this.id = id;
+		Name = name;
+	}
+	
+	
+}
