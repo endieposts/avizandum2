92d053a2aab08c02c0680d4b4605ce4dca041aa0
endieposts <endie@endie.net>
2022-08-04 19:49:21 +0100

    test checkin

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,3 +1,4 @@
 insert into TERRAIN (ID, NAME) values (1, 'Bog')
+insert into TERRAIN (ID, NAME) values (2, 'Forest')
 insert into DISTRICT (ID, TERRAIN_ID, NAME) values (1, 1, 'Bogtown')
 insert into DOMAIN (ID, NAME, OWNER_ID) values (1, 'Bogtopia', 1)
