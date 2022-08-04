9784498725d27dc797ddf3319cee5ce4fdd435b6
Endie <endie@local.com>
2022-08-04 19:28:53 +0100

    Added terrain

---
+++ src/main/java/com/endie/avizandum/domain/District.java
@@ -0,0 +1,49 @@
+package com.endie.avizandum.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class District {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private long terrainId;
+	
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getTerrainId() {
+		return terrainId;
+	}
+
+	public void setTerrainId(long terrainId) {
+		this.terrainId = terrainId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public District(long id, long terrainId, String name) {
+		super();
+		this.id = id;
+		this.terrainId = terrainId;
+		this.name = name;
+	}
+}
--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,1 +1,2 @@
 insert into TERRAIN (ID, NAME) values (1, 'Bog')
+insert into DISTRICT (ID, TERRAIN_ID, NAME) values (1, 1, 'Bogtown')
