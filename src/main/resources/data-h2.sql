0c2dccab8cba75342eb95abe8fa7a99f1fbea678
endieposts <endie@endie.net>
2022-08-04 19:54:00 +0100

    creds test

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,4 +1,5 @@
 insert into TERRAIN (ID, NAME) values (1, 'Bog')
 insert into TERRAIN (ID, NAME) values (2, 'Forest')
+insert into TERRAIN (ID, NAME) values (3, 'Plains')
 insert into DISTRICT (ID, TERRAIN_ID, NAME) values (1, 1, 'Bogtown')
 insert into DOMAIN (ID, NAME, OWNER_ID) values (1, 'Bogtopia', 1)
