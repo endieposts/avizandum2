56996527b125e3183bd475be5806a016bdcfb69a
endieposts <13526487+endieposts@users.noreply.github.com>
2022-08-07 15:58:04 +0100

    Vault connecting, db not picked up early enough yet

--- src/main/java/com/endie/avizandum/MaintenanceApplication.java
+++ src/main/java/com/endie/avizandum/MaintenanceApplication.java
@@ -3,8 +3,11 @@ package com.endie.avizandum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class MaintenanceApplication {
 	
