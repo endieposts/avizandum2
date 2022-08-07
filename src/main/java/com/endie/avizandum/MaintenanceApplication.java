2a4336a2b42e4bf7fe8755d69990d85c9bb1ec18
endieposts <13526487+endieposts@users.noreply.github.com>
2022-08-07 17:03:41 +0100

    Reading env variables from in app props

--- src/main/java/com/endie/avizandum/MaintenanceApplication.java
+++ src/main/java/com/endie/avizandum/MaintenanceApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
 @SpringBootApplication
