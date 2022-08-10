e4bdecef0dd60f0aa02d1436f6dbb476897f5c1f
endieposts <13526487+endieposts@users.noreply.github.com>
2022-08-10 22:09:07 +0100

    Got junit test working with separate profile

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
-insert into avizandum2.TERRAIN (ID, NAME) values (1, 'Bog');
-insert into avizandum2.TERRAIN (ID, NAME) values (2, 'Forest');
-insert into avizandum2.TERRAIN (ID, NAME) values (3, 'Plains');
-insert into avizandum2.TERRAIN (ID, NAME) values (4, 'Desert');
-insert into avizandum2.TERRAIN (ID, NAME) values (5, 'Loch');
-insert into avizandum2.TERRAIN (ID, NAME) values (6, 'Ocean');
+insert into TERRAIN (ID, NAME) values (1, 'Bog');
+insert into TERRAIN (ID, NAME) values (2, 'Forest');
+insert into TERRAIN (ID, NAME) values (3, 'Plains');
+insert into TERRAIN (ID, NAME) values (4, 'Desert');
+insert into TERRAIN (ID, NAME) values (5, 'Loch');
+insert into TERRAIN (ID, NAME) values (6, 'Ocean');
 
-insert into avizandum2.DISTRICT (ID, DOMAIN_ID, TERRAIN_ID , NAME) values (1, 2, 1, 'Bogtown');
-insert into avizandum2.DISTRICT (ID, DOMAIN_ID, TERRAIN_ID , NAME) values (2, 1, 2, 'Tabula Rasa');
+insert into DISTRICT (ID, DOMAIN_ID, TERRAIN_ID , NAME) values (1, 2, 1, 'Bogtown');
+insert into DISTRICT (ID, DOMAIN_ID, TERRAIN_ID , NAME) values (2, 1, 2, 'Tabula Rasa');
 
-insert into avizandum2.DOMAIN (ID, NAME, OWNER_ID) values (2, 'Bogtopia', 2);
-insert into avizandum2.DOMAIN (ID, NAME, OWNER_ID) values (1, 'Unowned', 1);
+insert into DOMAIN (ID, NAME, OWNER_ID) values (2, 'Bogtopia', 2);
+insert into DOMAIN (ID, NAME, OWNER_ID) values (1, 'Unowned', 1);
