220fbebfc503905746399c117789426bb5c031e3
endieposts <13526487+endieposts@users.noreply.github.com>
2022-08-21 21:01:17 +0100

    Login endpoint working and giving jwt token

--- src/main/java/com/endie/avizandum/SecurityConfig.java
+++ src/main/java/com/endie/avizandum/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.endie.avizandum;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,9 +17,32 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+    
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+		http.csrf()
+		.disable()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authorizeRequests()
+		.antMatchers("/login").permitAll()
+		.antMatchers(AUTH_WHITELIST).permitAll()
+		.anyRequest()
+		.authenticated();
 	}
 	
 	@Bean
@@ -32,4 +58,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new InMemoryUserDetailsManager(user);
 	}
 	
+	@Bean
+	public AuthenticationManager getAuthenticationManager() throws Exception {
+		return authenticationManager();
+	}
 }
