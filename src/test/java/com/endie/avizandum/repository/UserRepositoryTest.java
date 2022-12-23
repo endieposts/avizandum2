a0c4c0b211f57341a4ae561e2e9f1ea4dc527181
endieposts <13526487+endieposts@users.noreply.github.com>
2022-12-23 14:39:56 +0000

    This is a message

--- src/test/java/com/endie/avizandum/repository/UserRepositoryTest.java
+++ src/test/java/com/endie/avizandum/repository/UserRepositoryTest.java
@@ -23,9 +23,9 @@ public class UserRepositoryTest {
     }
     
     @Test
-    public void should_store_a_district() {
+    public void should_store_a_user() {
     	
-        AppUser AppUser = appUserRepository.save(new AppUser("User 1", "password", "player"));
+        AppUser AppUser = appUserRepository.save(new AppUser("User 1", "password", "default"));
         
 
         assertThat(AppUser).hasFieldOrPropertyWithValue("userName", "User 1");
@@ -34,12 +34,12 @@ public class UserRepositoryTest {
     }
     
     @Test
-    public void should_find_all_districts() {
+    public void should_find_all_users() {
  
 
-    	AppUser AppUser = appUserRepository.save(new AppUser("User 2", "password", "player"));
-    	AppUser AppUser2 = appUserRepository.save(new AppUser("User 3", "password", "player"));
-    	AppUser AppUser3 = appUserRepository.save(new AppUser("User 4", "password", "player")); 
+    	AppUser AppUser = appUserRepository.save(new AppUser("User 2", "password", "default"));
+    	AppUser AppUser2 = appUserRepository.save(new AppUser("User 3", "password", "default"));
+    	AppUser AppUser3 = appUserRepository.save(new AppUser("User 4", "password", "default")); 
 
         Iterable<AppUser> AppUsers = appUserRepository.findAll();
 
@@ -47,10 +47,10 @@ public class UserRepositoryTest {
     }
 
     @Test
-    public void should_find_district_by_id() {
+    public void should_find_user_by_id() {
 
-    	AppUser AppUser = appUserRepository.save(new AppUser("User 5", "password", "player"));
-    	AppUser AppUser2 = appUserRepository.save(new AppUser("User 6", "password", "player"));
+    	AppUser AppUser = appUserRepository.save(new AppUser("User 5", "password", "default"));
+    	AppUser AppUser2 = appUserRepository.save(new AppUser("User 6", "password", "default"));
 
     	AppUser foundAppUser = appUserRepository.findById(AppUser2.getId()).get();
 
@@ -58,11 +58,11 @@ public class UserRepositoryTest {
     }
     
     @Test
-    public void should_update_district_by_id() {
-    	AppUser AppUser = appUserRepository.save(new AppUser("User 7", "password", "player"));
-    	AppUser AppUser2 = appUserRepository.save(new AppUser("User 8", "password", "player"));
+    public void should_update_user_by_id() {
+    	AppUser AppUser = appUserRepository.save(new AppUser("User 7", "password", "default"));
+    	AppUser AppUser2 = appUserRepository.save(new AppUser("User 8", "password", "default"));
 
-    	AppUser updatedAppUser = new AppUser("User 1", "password", "player");
+    	AppUser updatedAppUser = new AppUser("User 1", "password", "default");
 
     	AppUser foundAppUser = appUserRepository.findById(AppUser2.getId()).get();
     	foundAppUser.setUserName(updatedAppUser.getUserName());
@@ -75,9 +75,9 @@ public class UserRepositoryTest {
     }
     
     @Test
-    public void should_delete_all_districts() {
-    	AppUser AppUser = appUserRepository.save(new AppUser("User 9", "password", "player"));
-    	AppUser AppUser2 = appUserRepository.save(new AppUser("User 10", "password", "player"));
+    public void should_delete_all_users() {
+    	AppUser AppUser = appUserRepository.save(new AppUser("User 9", "password", "default"));
+    	AppUser AppUser2 = appUserRepository.save(new AppUser("User 10", "password", "default"));
 
     	appUserRepository.deleteAll();
 
