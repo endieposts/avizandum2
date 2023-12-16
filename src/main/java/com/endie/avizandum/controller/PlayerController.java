cb1d3fff9802a3c5cdaae909413fa2dc3468d8bf
endieposts <13526487+endieposts@users.noreply.github.com>
2023-12-16 18:16:06 +0000

    Add PlayerTurn repo

--- src/main/java/com/endie/avizandum/controller/PlayerController.java
+++ src/main/java/com/endie/avizandum/controller/PlayerController.java
@@ -1,6 +1,5 @@
 package com.endie.avizandum.controller;
 
-import com.endie.avizandum.domain.District;
 import com.endie.avizandum.domain.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/endie/avizandum/domain/PlayerTurn.java
+++ src/main/java/com/endie/avizandum/domain/PlayerTurn.java
@@ -1,4 +1,34 @@
 package com.endie.avizandum.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="player_turn")
 public class PlayerTurn {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    private long playerId;
+    private long turnNumber;
+
+    public long getTurnNumber() {
+        return turnNumber;
+    }
+
+    public void setTurnNumber(long turnNumber) {
+        this.turnNumber = turnNumber;
+    }
+
+    public long getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(long playerId) {
+        this.playerId = playerId;
+    }
 }
