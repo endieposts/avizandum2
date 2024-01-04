bd8a6dbe8aa46fcadebe665d6c9a0b4a3d4d8452
endieposts <13526487+endieposts@users.noreply.github.com>
2024-01-04 20:43:57 +0000

    BBuild initial turn component object

--- src/main/java/com/endie/avizandum/domain/TurnComponent.java
+++ src/main/java/com/endie/avizandum/domain/TurnComponent.java
@@ -1,4 +1,29 @@
 package com.endie.avizandum.domain;
 
 public class TurnComponent {
+    private long id;
+    private long turnNumber;
+    private long playerId;
+
+    public TurnComponent(long id, long turnNumber, long playerId) {
+        this.id = id;
+        this.turnNumber = turnNumber;
+        this.playerId = playerId;
+    }
+
+    public TurnComponent() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getTurnNumber() {
+        return turnNumber;
+    }
+
+    public long getPlayerId() {
+        return playerId;
+    }
 }
