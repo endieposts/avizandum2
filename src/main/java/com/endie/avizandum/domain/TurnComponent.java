26993c4ed5cc427e0b76d491b9e964eb7f17ec8e
endieposts <13526487+endieposts@users.noreply.github.com>
2024-01-05 13:45:52 +0000

    Add turn component payload

--- src/main/java/com/endie/avizandum/domain/TurnComponent.java
+++ src/main/java/com/endie/avizandum/domain/TurnComponent.java
@@ -4,6 +4,8 @@ public class TurnComponent {
     private long id;
     private long turnNumber;
     private long playerId;
+    private String turnAction;
+
 
     public TurnComponent(long id, long turnNumber, long playerId) {
         this.id = id;
@@ -25,5 +27,13 @@ public class TurnComponent {
 
     public long getPlayerId() {
         return playerId;
+    }
+
+    public String getTurnAction() {
+        return turnAction;
+    }
+
+    public void setTurnAction(String turnAction) {
+        this.turnAction = turnAction;
     }
 }
