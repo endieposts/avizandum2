d3408f69dd9d007a9a6178e6678b3a09f78f1047
endieposts <13526487+endieposts@users.noreply.github.com>
2024-01-08 17:28:23 +0200

    Add player turn component controller

---
+++ src/main/java/com/endie/avizandum/controller/TurnComponentController.java
@@ -0,0 +1,12 @@
+package com.endie.avizandum.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TurnComponentController {
+    
+    @Autowired
+    private TurnComponentRepository repository;
+
+}
