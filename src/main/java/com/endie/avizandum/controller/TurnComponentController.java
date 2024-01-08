d461883813be5232d27c58b9d53cd0f73e54d0d9
endieposts <13526487+endieposts@users.noreply.github.com>
2024-01-08 17:32:48 +0200

    Fix broken repo import

--- src/main/java/com/endie/avizandum/controller/TurnComponentController.java
+++ src/main/java/com/endie/avizandum/controller/TurnComponentController.java
@@ -1,5 +1,6 @@
 package com.endie.avizandum.controller;
 
+import com.endie.avizandum.repository.TurnComponentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
