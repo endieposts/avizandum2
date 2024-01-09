c953a41646657fef931314cd264ca07aebec4084
endieposts <13526487+endieposts@users.noreply.github.com>
2024-01-09 20:10:01 +0200

    Add player turn controller get all method

--- src/main/java/com/endie/avizandum/controller/TurnComponentController.java
+++ src/main/java/com/endie/avizandum/controller/TurnComponentController.java
@@ -2,6 +2,7 @@ package com.endie.avizandum.controller;
 
 import com.endie.avizandum.repository.TurnComponentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class TurnComponentController {
 
     @Autowired
     private TurnComponentRepository repository;
+
+    @RequestMapping("/turnComponents")
+    public Iterable<com.endie.avizandum.domain.TurnComponent> getTurnComponents() {
+        return repository.findAll();
+    }
 
 }
