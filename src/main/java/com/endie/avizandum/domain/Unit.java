e840187cc191449d1fc168ddd6f73ed61abea196
endieposts <13526487+endieposts@users.noreply.github.com>
2024-01-12 13:03:31 +0000

    Add initial unit class

--- src/main/java/com/endie/avizandum/domain/Unit.java
+++ src/main/java/com/endie/avizandum/domain/Unit.java
@@ -1,4 +1,51 @@
 package com.endie.avizandum.domain;
 
 public class Unit {
+    private String name;
+    private long id;
+    private long playerId;
+    private long[] unitComponentIds;
+
+    public Unit(String name, long id, long playerId, long[] unitComponentIds) {
+        this.name = name;
+        this.id = id;
+        this.playerId = playerId;
+        this.unitComponentIds = unitComponentIds;
+    }
+
+    public Unit() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(long playerId) {
+        this.playerId = playerId;
+    }
+
+    public long[] getUnitComponentIds() {
+        return unitComponentIds;
+    }
+
+    public void setUnitComponentIds(long[] unitComponentIds) {
+        this.unitComponentIds = unitComponentIds;
+    }
 }
