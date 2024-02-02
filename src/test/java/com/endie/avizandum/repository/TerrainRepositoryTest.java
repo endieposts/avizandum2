763b5deeed26e56df6ac86db1804d7d4bd11acd2
endieposts <13526487+endieposts@users.noreply.github.com>
2024-02-02 21:16:28 +0000

    Update terrain delete by id

--- src/test/java/com/endie/avizandum/repository/TerrainRepositoryTest.java
+++ src/test/java/com/endie/avizandum/repository/TerrainRepositoryTest.java
@@ -68,7 +68,17 @@ public class TerrainRepositoryTest {
         assertThat(checkTerrain.getId()).isEqualTo(terrain2.getId());
         assertThat(checkTerrain.getName()).isEqualTo(updatedTerrain.getName());
     }
-    
+
+    @Test
+    public void should_delete_district_by_id() {
+    	Terrain terrain = terrainRepository.save(new Terrain("Test terrain name 8"));
+    	Terrain terrain2 = terrainRepository.save(new Terrain("Test terrain name 9"));
+
+    	terrainRepository.deleteById(terrain2.getId());
+
+        assertThat(terrainRepository.findAll()).hasSize(1).contains(terrain);
+    }
+
     @Test
     public void should_delete_all_districts() {
     	Terrain terrain = terrainRepository.save(new Terrain("Test terrain name 8"));
