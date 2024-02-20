6a65c7faa2cdf454d87f88650debbeb3badf0e6d
endieposts <13526487+endieposts@users.noreply.github.com>
2024-02-20 10:08:51 +0000

    BROKEN - trying to add AWS SQS service

---
+++ src/main/java/com/endie/avizandum/service/QueueService.java
@@ -0,0 +1,11 @@
+package com.endie.avizandum.service;
+package com.amazonaws.auth.AWSCredentials;
+package com.amazonaws.auth.BasicAWSCredentials;
+
+public class QueueService {
+
+    AWSCredentials credentials = new BasicAWSCredentials(
+            "<AWS accesskey>",
+            "<AWS secretkey>"
+    );
+}
