a635a5b28b2b6a6f5209c1f2101e7e60a359e30e
endieposts <13526487+endieposts@users.noreply.github.com>
2024-02-21 17:48:02 +0000

    Resolved awssdk pom issues by adding explicit version to secrets manager and reloading project after mvn dependency:resolve

--- src/main/java/com/endie/avizandum/service/QueueService.java
+++ src/main/java/com/endie/avizandum/service/QueueService.java
@@ -1,9 +1,22 @@
 package com.endie.avizandum.service;
-import org.springframework.stereotype.Service;
 
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SqsException;
+import software.amazon.awssdk.services.sqs.paginators.ListQueuesIterable;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 
 @Service
 public class QueueService {
 
+    //instantiate connection to AWS
+    SqsClient sqsClient = SqsClient.builder()
+            .region(Region.US_WEST_2)
+            .build();
+
+    public void addTurnToQueue() {
+
+    }
 
 }
