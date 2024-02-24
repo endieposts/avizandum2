f180ad9d74017e831f111327d57f00a7aa4f1a3d
endieposts <13526487+endieposts@users.noreply.github.com>
2024-02-24 16:48:23 +0000

    refactor for sleeker AWS approach

--- src/main/java/com/endie/avizandum/service/QueueService.java
+++ src/main/java/com/endie/avizandum/service/QueueService.java
@@ -1,66 +1,34 @@
 package com.endie.avizandum.service;
 
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sqs.*;
-import software.amazon.awssdk.services.sqs.model.SqsException;
-import software.amazon.awssdk.services.sqs.paginators.ListQueuesIterable;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.*;
 
 @Service
 public class QueueService {
+    private final SqsClient sqsClient;
+    private final String queueUrl;
 
-    //instantiate connection to AWS
-    SqsClient sqsClient = SqsClient.builder()
-            .region(Region.US_WEST_2)
-            .build();
-
-    public void addTurnToQueue() {
-
-        //Get the sqs queue url
-        String queueUrl = getQueueUrl("avizandum-queue");
-
+    public QueueService(String queueUrl) {
+        this.sqsClient = SqsClient.create();
+        this.queueUrl = queueUrl;
     }
 
-    private String getQueueUrl(String s) {
-        try {
-            ListQueuesIterable listQueues = sqsClient.listQueuesPaginator();
-
-            for (software.amazon.awssdk.services.sqs.model.ListQueuesResponse queue : listQueues) {
-                if (queue.queueUrls().contains(s)) {
-                    return queue.queueUrls().get(0);
-                }
-            }
-        } catch (SqsException e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
-        }
-        return "";
+    public void addPlayerTurn(String playerTurn) {
+        SendMessageRequest sendMessageRequest = SendMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .messageBody(playerTurn)
+                .build();
+        sqsClient.sendMessage(sendMessageRequest);
     }
 
-    public void sendMessage(String queueUrl, String message) {
-        try {
-            sqsClient.sendMessage(s -> s.queueUrl(queueUrl).messageBody(message));
-        } catch (SqsException e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
-        }
-    }
-
-    public void receiveMessage(String queueUrl) {
-        try {
-            sqsClient.receiveMessage(r -> r.queueUrl(queueUrl));
-        } catch (SqsException e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
-        }
-    }
-
-    public void deleteMessage(String queueUrl, String receiptHandle) {
-        try {
-            sqsClient.deleteMessage(d -> d.queueUrl(queueUrl).receiptHandle(receiptHandle));
-        } catch (SqsException e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
-        }
+    public String readPlayerTurn() {
+        ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .maxNumberOfMessages(1)
+                .build();
+        ReceiveMessageResponse receiveMessageResponse = sqsClient.receiveMessage(receiveMessageRequest);
+        Message message = receiveMessageResponse.messages().get(0);
+        return message.body();
     }
 }
