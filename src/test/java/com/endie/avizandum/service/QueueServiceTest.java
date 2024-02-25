b305a4dd1aeee38a0f58037b733ee42c73fdbf93
endieposts <13526487+endieposts@users.noreply.github.com>
2024-02-25 11:27:47 +0000

    kick off test class for QueueService

---
+++ src/test/java/com/endie/avizandum/service/QueueServiceTest.java
@@ -0,0 +1,7 @@
+package com.endie.avizandum.service;
+
+public class QueueServiceTest {
+    //todo - write tests for QueueService
+    //todo - remember to mock the SqsClient
+    //todo - remember to mock the SqsClient methods
+}
